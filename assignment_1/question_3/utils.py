170217762edf580fc504c1a5a93b30d9437a68ba
supreeths <srinath1@ualberta.ca>
2023-03-05 12:58:05 -0700

    Code cleanup

--- assignment_1/question_3/utils.py
+++ assignment_1/question_3/utils.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum, auto
 
 import cv2
 import numpy as np
@@ -22,100 +23,71 @@ def run_length_decode(_encoded_data):
     return np.array(_decoded_data).reshape(64, )
 
 
-def zigzag_scan(matrix):
-    UP, DOWN, LEFT, RIGHT = range(4)
+class Direction(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
 
-    # Initialize variables for the current position and direction of traversal
-    row, col, direction = 0, 0, RIGHT
-    # Define a list to store the scanned values
+
+def _get_zig_zag_indices(direction, row, col):
+    # UP, DOWN, LEFT, RIGHT = range(4)
+    if direction == Direction.UP:
+        if row == 0:
+            col += 1
+            direction = Direction.LEFT
+        elif col == 7:
+            row += 1
+            direction = Direction.LEFT
+        else:
+            row -= 1
+            col += 1
+    elif direction == Direction.DOWN:
+        if row == 7:
+            direction = Direction.RIGHT
+            col += 1
+        else:
+            row += 1
+            direction = Direction.UP
+    elif direction == Direction.LEFT:
+        if col == 0 and row != 7:
+            row += 1
+            direction = Direction.UP
+        elif row == 7:
+            col += 1
+            direction = Direction.UP
+        else:
+            row += 1
+            col -= 1
+            direction = Direction.LEFT
+    elif direction == Direction.RIGHT:
+        if col == 7:
+            direction = Direction.DOWN
+            row += 1
+        else:
+            col += 1
+            direction = Direction.LEFT
+    return direction, row, col
+
+
+def zigzag_scan(matrix):
+    row, col, direction = 0, 0, Direction.RIGHT
     scanned_values = []
-    # Iterate over each element in the matrix
     for _ in range(8 * 8):
-        # Append the current element to the scanned values list
         scanned_values.append(matrix[row][col])
-        # Move to the next element based on the current direction of traversal
-        if direction == UP:
-            if row == 0:
-                col += 1
-                direction = LEFT
-            elif col == 7:
-                row += 1
-                direction = LEFT
-            else:
-                row -= 1
-                col += 1
-        elif direction == DOWN:
-            if row == 7:
-                direction = RIGHT
-                col += 1
-            else:
-                row += 1
-                direction = UP
-        elif direction == LEFT:
-            if col == 0 and row != 7:
-                row += 1
-                direction = UP
-            elif row == 7:
-                col += 1
-                direction = UP
-            else:
-                row += 1
-                col -= 1
-                direction = LEFT
-        elif direction == RIGHT:
-            if col == 7:
-                direction = DOWN
-                row += 1
-            else:
-                col += 1
-                direction = LEFT
-    return np.array(scanned_values).reshape((8,8))
+        direction, row, col = _get_zig_zag_indices(direction, row, col)
+    return np.array(scanned_values).reshape((8, 8))
 
 
 def undo_zigzag_scan(matrix):
-    UP, DOWN, LEFT, RIGHT = range(4)
-    row, col, direction = 0, 0, RIGHT
+    row, col, direction = 0, 0, Direction.RIGHT
     un_zigzag_matrix = np.zeros((8, 8))
     matrix = matrix.reshape(64, )
     un_zigzag_matrix[row][col] = matrix[0]
     for c, value in enumerate(matrix):
         if c == 0:
             continue
-        if direction == UP:
-            if row == 0:
-                col += 1
-                direction = LEFT
-            elif col == 7:
-                row += 1
-                direction = LEFT
-            else:
-                row -= 1
-                col += 1
-        elif direction == DOWN:
-            if row == 7:
-                direction = RIGHT
-                col += 1
-            else:
-                row += 1
-                direction = UP
-        elif direction == LEFT:
-            if col == 0 and row != 7:
-                row += 1
-                direction = UP
-            elif row == 7:
-                col += 1
-                direction = UP
-            else:
-                row += 1
-                col -= 1
-                direction = LEFT
-        elif direction == RIGHT:
-            if col == 7:
-                direction = DOWN
-                row += 1
-            else:
-                col += 1
-                direction = LEFT
+        direction, row, col = _get_zig_zag_indices(direction, row, col)
         un_zigzag_matrix[row][col] = value
     return un_zigzag_matrix
 
